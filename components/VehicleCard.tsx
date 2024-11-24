110a54efc00a9e02feceb3d35a7fd8cbdc4a23e5
David Marshall <dlmarshall3@protonmail.com>
2024-11-24 15:54:57 -0600

    changing button colors

--- components/VehicleCard.tsx
+++ components/VehicleCard.tsx
@@ -18,7 +18,7 @@ export default function VehicleDashboardCard({
 }: VehicleDashboardCardProps) {
   const { imageUrl, year, make, model, clientName, _id } = vehicleData;
   return (
-    <div className="mb-4 mr-12 flex h-full w-full flex-col rounded-lg border p-4 text-center shadow-lg hover:bg-white hover:bg-opacity-75 lg:mb-0 lg:w-1/3">
+    <div className="mb-4 mr-12 flex h-full w-full flex-col rounded-lg border p-4 text-center shadow-lg hover:bg-white hover:bg-opacity-75 lg:mb-0 lg:w-1/2">
       <Image
         src={imageUrl}
         alt={`${make} ${model}`}
@@ -33,12 +33,12 @@ export default function VehicleDashboardCard({
       </div>
       <div className="mb-2">{clientName}</div>
       <Link href={`/vehicle/${_id}`}>
-        <button className="mb-5 w-3/4 rounded-full bg-yellow-500 p-2 shadow-lg hover:bg-yellow-300 lg:w-full">
+        <button className="mb-2 w-3/4 rounded-full bg-[#5d8075] p-2 text-white shadow-lg hover:bg-[#79927c] lg:w-full">
           View
         </button>
       </Link>
       <Link href={`/admin/update-vehicle/${_id}`}>
-        <button className="w-3/4 rounded-full bg-yellow-500 p-2 shadow-lg hover:bg-yellow-300 lg:w-full">
+        <button className="w-3/4 rounded-full bg-[#006b78] p-2 text-white shadow-lg hover:bg-[#0b7774] lg:w-full">
           Update
         </button>
       </Link>
